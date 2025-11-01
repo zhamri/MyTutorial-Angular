f2ba8055e60b0118d18f84661b652269f40740cc
zhamri <zhamri@gmail.com>
2025-11-01 16:00:18 +0800

    update user service

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+//
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class User {
+//
+// }
 
+import {Injectable, inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import { User } from '../models/user'; // 👈 import your model
+//
 @Injectable({
   providedIn: 'root',
 })
-export class User {
-  
+export class UserService {
+  private http = inject(HttpClient);
+  private readonly baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/users`);
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/${id}`);
+  }
 }
+
