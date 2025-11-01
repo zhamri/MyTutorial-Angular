d27a647614ae81a2b754998ac0349c9246ca22bd
zhamri <zhamri@gmail.com>
2025-11-01 16:33:19 +0800

    update component

--- src/app/get-api/get-api.ts
+++ src/app/get-api/get-api.ts
@@ -33,30 +33,3 @@ export class GetApi {
     });
   }
 }
-
-
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { User } from '../models/user';        // 👈 import the model
-// import { UserService } from '../services/user';
-//
-// @Component({
-//   selector: 'app-get-api',
-//   imports: [CommonModule],
-//   templateUrl: './get-api.html',
-//   styleUrl: './get-api.css',
-// })
-// export class GetApi implements OnInit{
-//   userList: User[] = [];                      // 👈 use the model here
-//
-//   constructor(private userService: UserService) {}
-//
-//   ngOnInit(): void {
-//     this.userService.getUsers().subscribe(users => {
-//       this.userList = users;
-//     });
-//   }
-// }
-
