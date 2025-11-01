7cc507140e2158f23e016effef2a8fa10fe8c07f
zhamri <zhamri@gmail.com>
2025-11-01 15:35:57 +0800

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GetApi} from './get-api/get-api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GetApi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
