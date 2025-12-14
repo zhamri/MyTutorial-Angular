4f3354d103d31bf604c6fe2853772e6a93311972
zhamri <zhamri@gmail.com>
2025-12-14 17:24:41 +0800

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Hello } from './hello/hello';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Hello],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('MyTutorial-Angular');
-}
+export class App {}
